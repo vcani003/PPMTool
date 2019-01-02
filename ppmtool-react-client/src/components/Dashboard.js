484f236c3042dcf1458740c82ffece1089791148
vcani003 <veronicacanido@gmail.com>
2019-01-01 19:43:03 -0500

    Get All Projects added to state in Redux

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react'
 import ProjectItem from './Project/ProjectItem';
 import CreateProjectButton from './Project/CreateProjectButton';
+import {connect} from "react-redux"
+import {getProjects} from "../actions/projectActions"
+import PropTypes from "prop-types"
 
 class Dashboard extends Component {
+//lifecycle hook
+componentDidMount(){
+  this.props.getProjects()
+}
+
   render() {
     return (
       <div className="projects">
@@ -24,4 +32,13 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard
+Dashboard.propTypes = {
+  project: PropTypes.object.isRequired,
+  getProjects: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+  project:state.project
+})
+
+export default connect(mapStateToProps, {getProjects})(Dashboard)
