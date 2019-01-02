8ddadc1760a8ced3b9a3ff058d513a6804806346
vcani003 <veronicacanido@gmail.com>
2019-01-01 20:03:35 -0500

    Mapping projects to show up on the dashboard

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react'
-import ProjectItem from './Project/ProjectItem';
-import CreateProjectButton from './Project/CreateProjectButton';
-import {connect} from "react-redux"
-import {getProjects} from "../actions/projectActions"
-import PropTypes from "prop-types"
+import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
+import CreateProjectButton from "./Project/CreateProjectButton";
+import { connect } from "react-redux";
+import { getProjects } from "../actions/projectActions";
+import PropTypes from "prop-types";
 
 class Dashboard extends Component {
-//lifecycle hook
-componentDidMount(){
-  this.props.getProjects()
-}
+  //lifecycle hook
+  componentDidMount() {
+    this.props.getProjects();
+  }
 
   render() {
+    const { projects } = this.props.project;
     return (
       <div className="projects">
         <div className="container">
@@ -19,26 +20,31 @@ componentDidMount(){
             <div className="col-md-12">
               <h1 className="display-4 text-center">Projects</h1>
               <br />
-              <CreateProjectButton/>
+              <CreateProjectButton />
               <br />
               <hr />
-              <ProjectItem/>
+
+              {projects.map(project => (
+                <ProjectItem key={project.id} project={project} />
+              ))}
             </div>
           </div>
         </div>
       </div>
-
-    )
+    );
   }
 }
 
 Dashboard.propTypes = {
   project: PropTypes.object.isRequired,
   getProjects: PropTypes.func.isRequired
-}
+};
 
 const mapStateToProps = state => ({
-  project:state.project
-})
+  project: state.project
+});
 
-export default connect(mapStateToProps, {getProjects})(Dashboard)
+export default connect(
+  mapStateToProps,
+  { getProjects }
+)(Dashboard);
