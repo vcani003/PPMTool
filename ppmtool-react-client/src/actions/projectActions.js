a923be58369faf36efc75d9778f9d02137c376e1
vcani003 <veronicacanido@gmail.com>
2019-01-03 21:39:48 -0500

    added in DeleteProject

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
   try {
@@ -24,6 +24,7 @@ export const getProjects = () => async dispatch => {
     payload: res.data
   });
 };
+
 export const getProject = (id, history) => async dispatch => {
   try {
     const res = await axios.get(`http://localhost:8080/api/project/${id}`);
@@ -35,3 +36,11 @@ export const getProject = (id, history) => async dispatch => {
     history.push("/dashboard");
   }
 };
+
+export const deleteProject = id => async dispatch => {
+  await axios.delete(`http://localhost:8080/api/project/${id}`);
+  dispatch({
+    type: DELETE_PROJECT,
+    payload:id
+  })
+}
